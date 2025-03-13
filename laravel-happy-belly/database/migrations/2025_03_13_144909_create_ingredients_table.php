40dfd1080e444674fd0a0e11fb5457a9e8707090
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-13 18:02:17 +0000

    added allergen column to table. Created seeder to populate table

--- laravel-happy-belly/database/migrations/2025_03_13_144909_create_ingredients_table.php
+++ laravel-happy-belly/database/migrations/2025_03_13_144909_create_ingredients_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('ingredients', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
             $table->string('food_group');
+            $table->boolean('allergen');
             $table->timestamps();
         });
     }
--- laravel-happy-belly/database/seeders/DatabaseSeeder.php
+++ laravel-happy-belly/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RecipeSeeder::class,
+            IngredientSeeder::class,
         ]);
     }
 }
