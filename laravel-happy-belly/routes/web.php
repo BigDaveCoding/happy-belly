d12f931a3a56613e6923dc555ea2cda9eca87079
BigDaveCoding <bigdavecoding@gmail.com>
2025-03-13 18:44:55 +0000

    changed '/' to return homepage as a view rather than welcome

--- laravel-happy-belly/routes/web.php
+++ laravel-happy-belly/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
 
 Route::get('/recipes', [RecipeController::class, 'all']);
